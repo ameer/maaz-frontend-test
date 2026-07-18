8341f8c984701acc5db5ad5110b85887d27c5555
Ameer Mousavi <a.mousavi@arasdp.ir>
2026-07-18 14:29:39 +0330

    feat: initialize Nuxt.js project with essential configurations and files
    - Add package.json with scripts and dependencies for Nuxt.js, Tailwind CSS, and Vue.js
    - Include favicon.ico for branding
    - Create robots.txt to manage web crawler access
    - Set up tsconfig.json for TypeScript support in Nuxt.js

---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: 'rtl',
+        lang: 'fa',
+      },
+    },
+  },
+  css: ['~/assets/css/main.css'],
+  ui: {
+    colorMode: false,
+  },
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: 'https://fakestoreapi.com',
+      appName: 'پیندُر',
+    },
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2026-07-16',
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ],
+    },
+  },
+  icon: {
+    customCollections: [
+      {
+        prefix: 'custom',
+        dir: './app/assets/icons',
+      },
+    ],
+  },
+})
