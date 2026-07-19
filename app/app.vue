c47d641f7dd2ed033e44412e3aa64ae174d1c1dd
Ameer Mousavi <a.mousavi@arasdp.ir>
2026-07-19 19:28:25 +0330

    feat: set locale and direction for UApp component

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import { fa_ir } from '@nuxt/ui/locale'
+</script>
+
 <template>
-  <UApp>
+  <UApp
+    :locale="fa_ir"
+    dir="rtl"
+  >
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
