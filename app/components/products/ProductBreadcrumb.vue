37f3c0f087faa31142c3a0b44f0263e220f2a209
Ameer Mousavi <a.mousavi@arasdp.ir>
2026-07-20 11:37:26 +0330

    feat: add breadcrumb SVG icon and refactor ProductBreadcrumb component

--- app/components/products/ProductBreadcrumb.vue
+++ app/components/products/ProductBreadcrumb.vue
@@ -1,23 +1,31 @@
-<template>
-  <div>
-    <!-- Hierarchy Breadcrumb -->
-    <div class="flex items-center gap-2 text-sm text-slate-500 font-medium">
-      <NuxtLink
-        to="/products"
-        class="hover:text-slate-800 transition-colors"
-      >لیست محصولات
-      </NuxtLink>
-      <UIcon
-        name="i-mdi-chevron-left"
-        class="w-4 h-4"
-      />
-      <span class="text-rose-500">{{ category }}</span>
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
-defineProps<{
+import type { BreadcrumbItem } from '@nuxt/ui'
+
+const props = defineProps<{
   category: string
 }>()
+
+const items = ref<BreadcrumbItem[]>([
+  {
+    label: 'صفحه اصلی',
+    icon: 'i-custom-breadcrumb',
+    to: '/',
+  },
+  {
+    label: 'لیست محصولات',
+    icon: '',
+    to: '/products',
+  },
+  {
+    label: props.category,
+    icon: '',
+    to: `/products?categories=${props.category}`,
+  },
+])
 </script>
+
+<template>
+  <nav aria-label="مسیر صفحه">
+    <UBreadcrumb :items="items" />
+  </nav>
+</template>
